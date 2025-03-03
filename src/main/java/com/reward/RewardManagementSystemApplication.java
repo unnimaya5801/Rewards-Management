088c0376dbafe46a8f44d2ce6a405686329bacf8
unnimaya5801 <unnimayas5801@gmail.com>
2025-03-03 14:44:03 +0530

    chore: init

---
+++ src/main/java/com/reward/RewardManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.reward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardManagementSystemApplication.class, args);
+	}
+
+}
