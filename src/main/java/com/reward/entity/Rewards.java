65da8d33abf347251e4d84169b102eaab7372339
unnimaya5801 <unnimayas5801@gmail.com>
2025-03-04 16:59:28 +0530

    feat: add Rewards entity

---
+++ src/main/java/com/reward/entity/Rewards.java
@@ -0,0 +1,53 @@
+package com.reward.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "rewards")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Rewards {
+
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private UUID id;
+
+@Column(nullable = false, length = 255)
+private String name;
+
+@Column(columnDefinition = "TEXT")
+private String description;
+
+@Lob 
+@Column(nullable = true)
+private byte[] image;
+
+@Column(nullable = false)
+private Integer points;
+
+@Column(nullable = true)
+private LocalDateTime deletedAt;
+
+@Column(updatable = false)
+private LocalDateTime createdAt;
+private LocalDateTime updatedAt;
+
+@PrePersist
+protected void onCreate() {
+this.createdAt = LocalDateTime.now();
+this.updatedAt = LocalDateTime.now();
+}
+
+@PreUpdate
+protected void onUpdate() {
+this.updatedAt = LocalDateTime.now();
+}
+}
+
+
